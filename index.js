345382bb92659212b713e654898bd3526b34b7fd
Pooja123-sharma <117913332+Pooja123-sharma@users.noreply.github.com>
2022-12-27 14:15:40 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
+// concat
+let a = [43,56,70];
+let b =[65,32,76];
+onsole.log(a.concat(b));
 
+//filter
+let x =[59,40,57];
+console.log(x.filter(x => x.length > x));
+
+//fill
+let y =[1,2,3,4];
+console.log(y.fill(0,2,4));
+
+//push
+let v =[78,45,23,10];
+d = [];
+console.log(v.push([]));
+
+//pop
+let n =[4,7,6,9];
+console.log(n.pop());
+
+// tostring
+let k =[56,89,12];
+console.log(k.tostring());
+
+//length
+let listA = [1,2,3];
+let l;istB = [6];
+console.log(listA.length);
